cd0235215583a41cd20d26fa40954a30cd5e9260
Robert Durica <r.durica@gmail.com>
2022-06-28 22:56:36 +0200

    Added: Collections

--- learn-swift/main.swift
+++ learn-swift/main.swift
@@ -8,9 +8,22 @@
 import Foundation
 
 let john = Employee(name: "John", surname: "Doe")
+let emily = Employee(name: "Emily", surname: "Doeson")
 
 do {
     try john.swim()
 } catch AppErrors.FirstError {
-    print("Oops... John cannot swim")
+    
+    /// With \() you can add variable into string
+    print("Oops... \(john.name) cannot swim")
+}
+
+/// Collections
+var employees = Array<Employee>()
+employees.append(john)
+employees.append(emily)
+
+/// Filter from collection
+var filteredJohn = employees.filter{ emp in
+    return emp.name == "John"
 }
